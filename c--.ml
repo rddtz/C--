0ee12bf6f0b8455294b57822bd94d4945f39fdb0
Rayan Raddatz <rayandematos@gmail.com>
2025-06-04 16:08:21 -0300

    evaluation of binary ops and now the entry is right

--- c--.ml
+++ c--.ml
@@ -1,92 +1,117 @@
 (* Operações numéricas *)
-type bop =  
+type bop =
   | Sum | Sub | Mul | Div   (* operações aritméticas *)
   | Eq  | Neq | Lt | Gt   (* operações relacionais  *)
-  | And | Or   (* operações lógicas *) 
-  
+  | And | Or   (* operações lógicas *)
+
 (*  Tipos *)
-type tipo = 
+type tipo =
   | TyInt
   | TyBool
   | TyRef of tipo
   | TyUnit
-    
+
 (*  Expressões *)
-type expr = 
+type expr =
   | Num of int
-  | Bool of bool 
+  | Bool of bool
   | Id of string
-  | If of expr * expr * expr 
+  | If of expr * expr * expr
   | Binop of bop * expr * expr
-  | Wh of expr * expr 
-  | Asg of expr * expr 
-  | Let of string * tipo * expr * expr 
+  | Wh of expr * expr
+  | Asg of expr * expr
+  | Let of string * tipo * expr * expr
   | New of expr
-  | Deref of expr 
+  | Deref of expr
   | Unit
   | Seq of expr * expr
   | Read
   | Print of expr
-  
-  
+
 (* value : expr -> bool *)
 let rec value (e:expr) : bool =
-  match e with 
-  | Bool b   -> true 
+  match e with
+  | Bool b   -> true
   | Num n    -> true
-  | Unit     -> true 
+  | Unit     -> true
   | Id z     -> true
   | _ -> false
 
 
 
-let rec step (e:expr) : expr option = 
+let rec num_expr (e:expr) : int =
+  match e with
+  | Num n -> n
+
+let rec bool_expr (e:expr) : bool =
+  match e with
+  | Bool b -> b
+
+
+
+let rec step ((e : expr), (mem : int list), (input : int list), (output : int list)): expr option =
   match e with
   | Num n             -> None (* Values don't move *)
   | Bool b            -> None
   | Unit              -> None
   | Id z              -> None
-  | Binop (op,e1,e2) -> (match step e1 with
-      | None      -> (match step e2 with 
-          | None     -> (match op with 
-              | Sum -> Some (Num (e1 + e2))
-              | Sub -> Some (Num (e1 - e2)))
+  (* Operações Binárias *)
+  | Binop (op,e1,e2) -> (match step (e1, mem, input, output) with
+      | None      -> (match step (e2, mem, input, output) with
+          | None     -> (match op with
+              | Sum -> Some (Num (num_expr(e1) + num_expr(e2)))
+              | Sub -> Some (Num (num_expr(e1) - num_expr(e2)))
+              | Mul -> Some (Num (num_expr(e1) * num_expr(e2)))
+              | Div -> Some (Num (num_expr(e1) / num_expr(e2)))
+              | Eq  -> Some (Bool (num_expr(e1) == num_expr(e2)))
+              | Neq -> Some (Bool (num_expr(e1) != num_expr(e2)))
+              | Lt  -> Some (Bool (num_expr(e1) < num_expr(e2)))
+              | Gt  -> Some (Bool (num_expr(e1) > num_expr(e2)))
+              | And -> Some (Bool (bool_expr(e1) && bool_expr(e2)))
+              | Or  -> Some (Bool (bool_expr(e1) || bool_expr(e2)))
+            )
           | Some e2' -> Some (Binop (op, e1, e2')))
       | Some e1'  -> Some (Binop (op,e1',e2))
     )
-  | _ -> (Num -1) 
-         
-(* steps : expr -> expr *) 
-let rec steps (e:expr) : expr =
-  match step e with
-  | None -> e
-  | Some e' -> steps e' 
+  (* If *)
+  | If (e1, e2, e3) -> (match step (e1, mem, input, output) with
+      | None -> (match e1 with
+          | Bool true  -> Some e2
+          | Bool false -> Some e3)
+      | Some e1' -> Some(If (e1', e2, e3))
+    )
+  | _ -> Some (Num (-1))
 
-                     
+(* steps : expr -> expr *)
+let rec steps ((e : expr), (mem : int list), (input : int list), (output : int list)): expr =
+  match step (e, mem, input, output) with
+  | None -> e
+  | Some e' -> steps (e', mem, input, output)
+
+
           (*  TEST CASE
-           
-            let  x: int     =  read() in 
-            let  z: ref int = new x in 
-            let  y: ref int = new 1 in 
-            
+
+            let  x: int     =  read() in
+            let  z: ref int = new x in
+            let  y: ref int = new 1 in
+
             (while (!z > 0) (
                    y :=  !y * !z;
                    z :=  !z - 1);
-            print (! y))     
+            print (! y))
 
-*) 
+*)
+
 
 let cndwhi = Binop(Gt, Deref (Id "z"),Num 0)
 let asgny = Asg(Id "y", Binop(Mul, Deref (Id "y"),Deref(Id "z")))
 let asgnz = Asg(Id "z", Binop(Sub, Deref (Id "z"),Num 1))
-let bdwhi = Seq(asgny, asgnz) 
+let bdwhi = Seq(asgny, asgnz)
 let whi = Wh(cndwhi, bdwhi)
 let prt = Print(Deref (Id "y"))
 let seq = Seq(whi, prt)
-    
-let fat = Let("x", TyInt, Read, 
-              Let("z", TyRef TyInt, New (Id "x"), 
-                  Let("y", TyRef TyInt, New (Num 1),
-                      seq))) 
 
-    
+let fat = Let("x", TyInt, Read,
+              Let("z", TyRef TyInt, New (Id "x"),
+                  Let("y", TyRef TyInt, New (Num 1),
+                      seq)))
