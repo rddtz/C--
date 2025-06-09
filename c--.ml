5ece80bcab65942ff11b8411ff078918724e22e5
Rayan Raddatz <rayandematos@gmail.com>
2025-06-09 17:03:27 -0300

    now with memory

--- c--.ml
+++ c--.ml
@@ -1,3 +1,5 @@
+open List
+
 (* Operações numéricas *)
 type bop =
   | Sum | Sub | Mul | Div   (* operações aritméticas *)
@@ -49,10 +51,17 @@ let rec num_expr (e:expr) : int =
 let rec bool_expr (e:expr) : bool =
   match e with
   | Bool b -> b
+  
+let rec id_expr (e:expr) : string =
+  match e with
+  | Id z -> z
+  
+    
 
 
+module StringMap = Map.Make(String)
 
-let rec step ((e : expr), (m : int list), (ip : int list), (out : int list)) : expr option * int list * int list * int list =
+let rec step ((e : expr), (m : (string * expr) list), (ip : int list), (out : int list)) : expr option * (string * expr) list * int list * int list =
   match e with
 
     (* Values *)
@@ -88,11 +97,43 @@ let rec step ((e : expr), (m : int list), (ip : int list), (out : int list)) : e
           | (Bool false, m'', ip'', out'') -> (Some e3, m'', ip'', out'')  
         )
       | (Some e1', m', ip', out')  -> (Some(If (e1', e2, e3)), m', ip', out') 
+    ) 
+    
+  (* Input/Output *)
+  | Read -> (Some (Num (List.hd ip)), m, (List.tl ip), out) 
+  | Print (e1) -> (match step (e1, m, ip, out) with
+      | (None, m', ip', out') -> (Some Unit, m, ip, out @ [(num_expr e1)])
+      | (Some e1', m', ip', out')  -> (Some (Print e1'), m', ip', out')  
     )
+    
+  (* Sequência *)
+  | Seq(e1,e2) -> (match step (e1, m, ip, out) with
+      | (None, m', ip', out') -> (Some e2, m', ip', out')
+      | (Some e1', m', ip', out')  -> (Some (Seq(e1',e2)), m', ip', out') 
+    )
+  
+  (* While *)
+  | Wh(e1, e2) -> (Some(If (e1, Wh(e1, e2), Unit)), m, ip, out)
+                  
+  (* Atribuição de variáveis *)
+  | Asg(e1, e2) -> (match step (e1, m, ip, out) with
+      | (None, m', ip', out') -> (match step (e2, m', ip', out') with
+          | (None, m'', ip'', out'')  -> (Some Unit, (id_expr(e1), e2) :: m'', ip'', out'')  
+          | (Some e2', m'', ip'', out'') -> (Some(Asg(e1, e2')), m'', ip'', out'')  
+        )
+      | (Some e1', m', ip', out')  -> (Some(Asg (e1', e2)), m', ip', out') 
+    )
+    
+  (* Derreferência de variáveis *)
+  | Deref(e1) -> (match step (e1, m, ip, out) with
+      | (None, m', ip', out') -> (Some (assoc (id_expr(e1)) m), m', ip', out')
+      | (Some e1', m', ip', out')  -> (Some (Deref(e1')), m', ip', out') 
+    )
+    
   | _ -> (Some (Num (-1)), m, ip, out) 
-
+         
 (* steps : expr -> expr *)
-let rec steps ((e : expr), (m : int list), (ip : int list), (out : int list)): expr * int list * int list * int list =
+let rec steps ((e : expr), (m : (string * expr) list), (ip : int list), (out : int list)): expr * (string * expr) list * int list * int list =
   match step (e, m, ip, out) with
   | (None, m', ip', out')   -> (e, m', ip', out')
   | (Some e', m', ip', out') -> steps (e', m', ip', out')
@@ -109,8 +150,11 @@ let rec steps ((e : expr), (m : int list), (ip : int list), (out : int list)): e
                    z :=  !z - 1);
             print (! y))
 
-*)
-
+*) 
+                                  
+let tif = If (Binop(Eq, (Num 1), (Num 1)), Print (Binop(Sum, (Num 2), (Num 3))), (Num 4))
+let teste = steps (Seq(tif, Print(Num(10))), [], [1], []) ;; 
+let teste2 = steps (Seq(Asg(Id "r", Num (10)), Deref(Id "r")), [], [1], []) ;; 
 
 let cndwhi = Binop(Gt, Deref (Id "z"),Num 0)
 let asgny = Asg(Id "y", Binop(Mul, Deref (Id "y"),Deref(Id "z")))
@@ -124,3 +168,4 @@ let fat = Let("x", TyInt, Read,
               Let("z", TyRef TyInt, New (Id "x"),
                   Let("y", TyRef TyInt, New (Num 1),
                       seq)))
+
